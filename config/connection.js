e6414100b85125bb1172b44b5d4ec568f8296bc3
hayesejasmine <hayesejasmine@gmail.com>
2023-03-01 23:47:10 -0500

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,12 @@
 const { connect, connection } = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/socialDB',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-);
+
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialDB';
+
+connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 module.exports = connection;
